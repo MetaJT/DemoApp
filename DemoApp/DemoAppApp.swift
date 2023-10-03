70afb8c2854a76d0bff08caa924439dbd8d7922a
MetaJT <jordantrotter3@gmail.com>
2023-10-03 13:33:40 -0500

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Jordan Trotter on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
